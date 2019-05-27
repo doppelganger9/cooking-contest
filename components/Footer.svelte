50f36995ceef8833d9ed9cd3c6da6abac7edbb31
David Lacourt <david.lacourt@gmail.com>
2019-05-27 15:35:10 +0200

    Adds disconnect as link in footer

--- components/Footer.svelte
+++ components/Footer.svelte
@@ -1,9 +1,22 @@
+<script>
+  import Disconnect from './Disconnect.svelte';
+
+  import { username } from '../stores/username.js';
+</script>
 <style>
   footer {
     width: 99%;
     margin: 0 auto;
     padding: 0;
   }
+  em {
+    color: blueviolet;
+    font-style: normal;
+  }
+
 </style>
 
-<footer>Made with 🧡 and Svelte by <a href="lacourt.dev">David Lacourt</a></footer>
+<footer>
+<p>Made with 🧡 and Svelte by <a href="lacourt.dev">David Lacourt</a></p>
+{#if $username}<p>Bonjour, <em>{$username}</em> <Disconnect button={false}/></p>{/if}
+</footer>
